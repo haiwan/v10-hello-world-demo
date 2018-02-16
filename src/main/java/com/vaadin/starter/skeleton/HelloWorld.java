f3245f541c34b5958ed90185934ac2e055280c91
haijian <haijian@vaadin.com>
2018-02-16 13:19:45 +0200

    added button click

---
+++ src/main/java/com/vaadin/starter/skeleton/HelloWorld.java
@@ -0,0 +1,27 @@
+package com.vaadin.starter.skeleton;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.polymertemplate.Id;
+import com.vaadin.flow.templatemodel.TemplateModel;
+import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.dependency.HtmlImport;
+import com.vaadin.flow.component.polymertemplate.PolymerTemplate;
+
+@Tag("hello-world")
+@HtmlImport("frontend://hello-world.html")
+public class HelloWorld extends PolymerTemplate<HelloWorld.HelloWorldModel> {
+
+    @Id("vaadinButton")
+    private Button vaadinButton;
+
+    public HelloWorld(){
+        vaadinButton.addClickListener(e->{
+           getElement().getParent().appendChild(new Label("Ouch!").getElement());
+        });
+    }
+
+    public interface HelloWorldModel extends TemplateModel {
+
+    }
+}
